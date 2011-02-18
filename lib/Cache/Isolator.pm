40be6169c1dca12a3517f8f96adcd8da969cdaf0
Masahiro Nagano <kazeburo@gmail.com>
2011-02-18 15:30:23 +0900

    initial commit

---
+++ lib/Cache/Isolator.pm
@@ -0,0 +1,32 @@
+package Cache::Isolator;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Cache::Isolator -
+
+=head1 SYNOPSIS
+
+  use Cache::Isolator;
+
+=head1 DESCRIPTION
+
+Cache::Isolator is
+
+=head1 AUTHOR
+
+Masahiro Nagano E<lt>kazeburo {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
